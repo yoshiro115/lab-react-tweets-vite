2751b7a1811bd1da1f3bfeadf229ea133425e914
vinceevogue <ferreiravincent115@gmail.com>
2025-03-11 16:25:18 +0100

    done

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -4,7 +4,7 @@ export const User = (user) => {
   return (
      <span className="user">
                 <span className="name">{user.name}</span>
-                <span className="handle">{user.handle}</span>
+                <span className="handle">@{user.handle}</span>
               </span>
   )
 }
